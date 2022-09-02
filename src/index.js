7bec76f07f9567271f7891c36337dde5556b2e60
NikDoe <nikd0e@yandex.ru>
2022-09-02 10:18:45 +0300

    feature(pizza): connecting to DB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { AppDataSource } from './db.js';
 
 const app = express();
 const PORT = process.env.PORT || 9001;
@@ -10,6 +11,12 @@ app.use(express.json());
 
 const start = async () => {
 	try {
+		await AppDataSource.initialize()
+			.then(() => {
+				console.log('база данных подключена');
+			})
+			.catch(error => console.log(error));
+
 		await app.listen(PORT, () => {
 			console.log(`сервер запущен на порту ${PORT}`);
 		});
