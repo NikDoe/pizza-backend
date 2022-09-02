a22e84e7eb5f393654d71f3296864388b9732145
NikDoe <nikd0e@yandex.ru>
2022-09-02 10:09:56 +0300

    feature(pizza): starting server
    - add PORT variable to .env file;

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello there :)');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 9001;
+
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+	try {
+		await app.listen(PORT, () => {
+			console.log(`сервер запущен на порту ${PORT}`);
+		});
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+start();
