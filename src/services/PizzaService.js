87cbb5257f3b412bab6abe8f6d0db302a7d8536e
NikDoe <nikd0e@yandex.ru>
2022-09-07 12:57:11 +0300

    feature(pizza): add searching functionality

--- src/services/PizzaService.js
+++ src/services/PizzaService.js
@@ -1,30 +1,32 @@
-import {pizzaRepo} from '../utils/utils.js';
+import { pizzaRepo } from '../utils/utils.js';
+import {Like} from "typeorm";
 
 class PizzaService {
-	async createPizza(pizza) {
+	async createPizza (pizza) {
 		const newPizza = pizzaRepo.create(pizza);
 		await pizzaRepo.save(newPizza);
 		return newPizza;
 	}
 
-	async getAllPizza({category, sortBy, order}) {
+	async getAllPizza ({ category, sortBy, order, search }) {
 		let allPizzas;
-		if (!category && !sortBy) {
-			allPizzas = await pizzaRepo.find()
+		if (!category && !sortBy && !search) {
+			allPizzas = await pizzaRepo.find();
 		}
-		if (category && !sortBy) {
-			allPizzas = await pizzaRepo.findBy({category})
+		if (category && sortBy && !search) {
+			allPizzas = await pizzaRepo.find({ where: { category }, order: { [sortBy]: order } });
 		}
-
-		if (!category && sortBy) {
-			allPizzas = await pizzaRepo.find({order: {[sortBy]: order}})
+		if (!category && sortBy && !search) {
+			allPizzas = await pizzaRepo.find({order: { [sortBy]: order } });
 		}
-		if (category && sortBy) {
-			allPizzas = await pizzaRepo.find({where:{category}, order: {[sortBy]: order}})
+		if (!category && sortBy && search) {
+			allPizzas = await pizzaRepo.find({ where: { title: Like(`%${search}%`) }, order: { [sortBy]: order } });
+		}
+		if (category && sortBy && search) {
+			allPizzas = await pizzaRepo.find({ where: { category, title: Like(`%${search}%`) }, order: { [sortBy]: order } });
 		}
 
 		return allPizzas;
-
 	}
 }
 
