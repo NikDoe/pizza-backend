797c5f8f2bee7aed1d6863dbeaadf401d810340e
NikDoe <nikd0e@yandex.ru>
2022-09-08 11:14:04 +0300

    refactor(pizza): no take & skip w/ categories on

--- src/services/PizzaService.js
+++ src/services/PizzaService.js
@@ -1,41 +1,39 @@
-import {pizzaRepo} from '../utils/utils.js';
-import {ILike} from "typeorm";
+import { pizzaRepo } from '../utils/utils.js';
+import { ILike } from 'typeorm';
 
 class PizzaService {
-	async createPizza(pizza) {
+	async createPizza (pizza) {
 		const newPizza = pizzaRepo.create(pizza);
 		await pizzaRepo.save(newPizza);
 		return newPizza;
 	}
 
-	async getAllPizza({category, sortBy, order, search, limit, page}) {
+	async getAllPizza ({ category, sortBy, order, search, limit, page }) {
 		let allPizzas;
 		const currentPage = page || 1;
 		const take = limit || 4;
 		const skip = currentPage * take - take;
 		if (!category && !sortBy && !search) {
-			allPizzas = await pizzaRepo.findAndCount({skip, take});
+			allPizzas = await pizzaRepo.findAndCount({ skip, take });
 		}
 		if (category && sortBy && !search) {
-			allPizzas = await pizzaRepo.findAndCount({where: {category}, order: {[sortBy]: order}, skip, take});
+			allPizzas = await pizzaRepo.findAndCount({ where: { category }, order: { [sortBy]: order } });
 		}
 		if (!category && sortBy && !search) {
-			allPizzas = await pizzaRepo.findAndCount({order: {[sortBy]: order}, skip, take});
+			allPizzas = await pizzaRepo.findAndCount({ order: { [sortBy]: order }, skip, take });
 		}
 		if (!category && sortBy && search) {
 			allPizzas = await pizzaRepo.findAndCount({
-				where: {title: ILike(`%${search}%`)},
-				order: {[sortBy]: order},
+				where : { title: ILike(`%${search}%`) },
+				order : { [sortBy]: order },
 				skip,
 				take
 			});
 		}
 		if (category && sortBy && search) {
 			allPizzas = await pizzaRepo.findAndCount({
-				where: {category, title: ILike(`%${search}%`)},
-				order: {[sortBy]: order},
-				skip,
-				take
+				where : { category, title: ILike(`%${search}%`) },
+				order : { [sortBy]: order }
 			});
 		}
 
